5ec7ec5898b65b77dcc8aff36e3567b714aaa378
乔克 <15036738726@163.com>
2023-04-11 22:56:26 +0800

    全局异常处理类,位置调整

--- ForumDemo/src/main/java/com/example/forumdemo/system/GlobalExceptionHandler.java
+++ ForumDemo/src/main/java/com/example/forumdemo/system/GlobalExceptionHandler.java
@@ -1,8 +1,5 @@
-package com.example.forumdemo.util;
-
-
-
-
+package com.example.forumdemo.system;
+import com.example.forumdemo.util.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -11,8 +8,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-
 /**
  * 全局异常处理
  */
