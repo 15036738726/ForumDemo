668353711c6ba92d3d08a5c2dc7ae13b89eb1e41
15036738726 <15036738726@163.com>
2023-06-21 16:22:15 +0800

    定时任务相关代码部分测试

--- ForumDemo/src/main/java/com/example/forumdemo/ForumDemoApplication.java
+++ ForumDemo/src/main/java/com/example/forumdemo/ForumDemoApplication.java
@@ -3,9 +3,12 @@ package com.example.forumdemo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan({"com.example.forumdemo.*.mapper"})
+@EnableScheduling
 public class ForumDemoApplication {
 
     public static void main(String[] args) {
